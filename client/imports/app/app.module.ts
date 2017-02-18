30bf126ec19b2f03a1e71a348f6da00ac4880b4d
kinwong <kinhangwong@gmail.com>
2017-02-19 00:43:22 +0800

    Remove sidebar.

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, OnInit, OnDestroy, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { Logger } from 'angular2-logger/core';
+import {SidebarModule} from 'ng-sidebar';
 
 import * as ngb from 'ng2-bootstrap';
  /**
@@ -19,7 +20,8 @@ import * as ngb from 'ng2-bootstrap';
     ngb.AlertModule.forRoot(),
     ngb.ButtonsModule.forRoot(),
     ngb.TooltipModule.forRoot(),
-    ngb.CollapseModule.forRoot()],
+    ngb.CollapseModule.forRoot(),
+    SidebarModule],
 
     providers: [Logger],
 })
