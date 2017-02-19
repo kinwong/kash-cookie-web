8b0e3f11ddee83eaed43849edc165b51549dd380
kinwong <kinhangwong@gmail.com>
2017-02-19 20:07:15 +0800

    Clean up packages.

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/meteor/index.d.ts" />
+/// <reference path="globals/rx/index.d.ts" />
+/// <reference path="globals/zone.js/index.d.ts" />
+/// <reference path="modules/winston/index.d.ts" />
